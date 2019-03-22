f9f6cd41f138484725e838391ab42e3426d17dd4
Victor <t0r0X@users.noreply.github.com>
2019-03-22 16:14:28 +0100

    Implement new environment variables default values

--- launch.sh
+++ launch.sh
@@ -4,22 +4,18 @@
 [[ "${SOCAT_DEBUG}" = "true" ]] && set -x
 
 # Create TCP wrapper file for ALLOW
-[[ -n "${SOCAT_ALLOW}" ]] || SOCAT_ALLOW=ALL
+[[ -z "${SOCAT_ALLOW}" ]] && SOCAT_ALLOW=ALL
 echo "socat-proxy: ${SOCAT_ALLOW}" > ~/socat.allow
 
 # Create TCP wrapper file for DENY
-[[ -n "${SOCAT_DENY}" ]] || SOCAT_DENY=NONE
+if [[ -z "${SOCAT_DENY}" ]] ; then
+    [[ "${SOCAT_ALLOW}" = ALL ]] && SOCAT_DENY=NONE || SOCAT_DENY=ALL
+fi
 echo "socat-proxy: ${SOCAT_DENY}" > ~/socat.deny
 
-# Validate source and target addresses
-if [[ -z "${SOCAT_SRC}" ]] ; then
-    echo "ERROR: $0: need 'socat' source address (what to export) in variable 'SOCAT_SRC'!"
-    exit 1
-fi
-if [[ -z "${SOCAT_TGT}" ]] ; then
-    echo "ERROR: $0: need 'socat' target address (where to export) in variable 'SOCAT_TGT'!"
-    exit 1
-fi
+# Reasonable defaults for source and target addresses regarding Docker Unix domain socket
+[[ -z "${SOCAT_SRC}" ]] && SOCAT_SRC='unix-connect:/var/run/docker.sock'
+[[ -z "${SOCAT_TGT}" ]] && SOCAT_TGT='TCP4-LISTEN:4550,fork'
 
 # Run socat command line
 socat ${SOCAT_OPTS} "${SOCAT_SRC}" "${SOCAT_TGT},tcpwrap=socat-proxy,allow-table=~/socat.allow,deny-table=~/socat.deny"
