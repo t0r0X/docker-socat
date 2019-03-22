7a39d79d8774345cc8932e9e0ce37d59e262d678
Victor <t0r0X@users.noreply.github.com>
2019-03-22 17:38:47 +0100

    Replace in file paths '~' with '/root'!

--- launch.sh
+++ launch.sh
@@ -5,17 +5,17 @@
 
 # Create TCP wrapper file for ALLOW
 [[ -z "${SOCAT_ALLOW}" ]] && SOCAT_ALLOW=ALL
-echo "socat-proxy: ${SOCAT_ALLOW}" > ~/socat.allow
+echo "socat-proxy: ${SOCAT_ALLOW}" > /root/socat.allow
 
 # Create TCP wrapper file for DENY
 if [[ -z "${SOCAT_DENY}" ]] ; then
     [[ "${SOCAT_ALLOW}" = ALL ]] && SOCAT_DENY=NONE || SOCAT_DENY=ALL
 fi
-echo "socat-proxy: ${SOCAT_DENY}" > ~/socat.deny
+echo "socat-proxy: ${SOCAT_DENY}" > /root/socat.deny
 
 # Reasonable defaults for source and target addresses regarding Docker Unix domain socket
 [[ -z "${SOCAT_SRC}" ]] && SOCAT_SRC='unix-connect:/var/run/docker.sock'
 [[ -z "${SOCAT_TGT}" ]] && SOCAT_TGT='TCP4-LISTEN:4550,fork'
 
 # Run socat command line
-exec socat ${SOCAT_OPTS} "${SOCAT_SRC}" "${SOCAT_TGT},tcpwrap=socat-proxy,allow-table=~/socat.allow,deny-table=~/socat.deny"
+exec socat ${SOCAT_OPTS} "${SOCAT_SRC}" "${SOCAT_TGT},tcpwrap=socat-proxy,allow-table=/root/socat.allow,deny-table=/root/socat.deny"
